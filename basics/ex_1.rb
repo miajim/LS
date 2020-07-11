7461f2c3c4c2cd659e8f5de64f1d32c170d6e624
Mia J <emailmiaj@gmail.com>
2020-07-11 18:26:43 +0000

    Completed Basics chapter exercises

--- basics/ex_1.rb
+++ basics/ex_1.rb
@@ -1,1 +1,8 @@
 # Basics Chapter, Exercise 1
+# 7/10/20
+
+=begin
+Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+=end 
+
+print "Mia" + " Jimenez"
---
+++ basics/ex_6.rb
@@ -0,0 +1,10 @@
+# Basics Chapter, Exercise 6
+# 7/11/20 
+
+=begin
+Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
+=end
+
+puts 3.14159**2
+puts 2.71828**2
+puts 6.02*10**23**2
