8d425d758031e69d2f326933accfbc1d905bc0e7
Mia J <emailmiaj@gmail.com>
2020-08-02 21:42:50 +0000

    Remove shifted files from prep

--- basics/ex_1.rb
+++
@@ -1,8 +0,0 @@
-# Basics Chapter, Exercise 1
-# 7/10/20
-
-=begin
-Add two strings together that, when concatenated, return your first and last name as your full name in one string.
-=end 
-
-print "Mia" + " Jimenez"
--- exercises/ex_7.rb
+++
@@ -1,7 +0,0 @@
-# Exercises Chapter, Exercise 7
-# 7/19/20
-
-# What's the major difference between an Array and a Hash?
-
-# Array: ordered list, each item is indexed starting with zero
-# Hash: "undordered" list, organized by key-value pairs; values can be accessed by key
