c5d3cfaa76748e4296f2b875e42dacd86769ed7f
Mia J <emailmiaj@gmail.com>
2020-08-25 14:37:20 +0000

    RB101 Small Problems - Easy 1 and PEDAC template

---
+++ pedac_template.rb
@@ -0,0 +1,27 @@
+# RB???-RB??? MAIN TITLE
+# SUBTITLE - Problem X: PROBLEM TITLE/NAME
+# DATE xx/xx/xx
+
+# PROBLEM STATEMENT
+
+=begin
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+PEDAC Template
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+UNDERSTAND THE PROBLEM
+Inputs
+Outputs 
+Rules/Implicit Requirements
+Clarifying Questions
+Mental Model (optional)
+
+EXAMPLES/TEST CASES
+
+DATA STRUCTURE
+Inputs
+Rules
+
+ALGORITHM
+=end
+
+# CODE
