b2b8726c88c31e33d0f4983ce23364bef16ee510
Mia J <emailmiaj@gmail.com>
2020-09-06 16:18:16 +0000

    Refactored RPS Bonus Features Assignment (based on TA feedback)

--- RB_101/lesson_two/rps_bonus_features_refactored.rb
+++ RB_101/lesson_two/rps_bonus_features_refactored.rb
@@ -17,18 +17,7 @@ MESSAGES = YAML.load_file('rps_bonus_messages.yml')
 
 NUM_WINS = 5
 
-RULES = <<-MSG
-  Scissors cuts Paper
-  Paper covers Rock
-  Rock crushes Lizard
-  Lizard poisons Spock
-  Spock smashes Scissors
-  Scissors decapitates Lizard
-  Lizard eats Paper
-  Paper disproves Spock
-  Spock vaporizes Rock
-  Rock crushes Scissors
-MSG
+RULES = MESSAGES["rules"]
 
 VALID_CHOICES = {
   'r' => "rock",
@@ -95,7 +84,7 @@ def let_user_choose
   choice = ''
   loop do
     prompt(messages("request_user_input"), false)
-    choice = Kernel.gets().chomp().downcase()
+    choice = gets.chomp.strip.downcase
     if VALID_CHOICES.key?(choice)
       choice = VALID_CHOICES[choice]
       break
@@ -146,7 +135,7 @@ end
 
 def someone_won?(scores_hash)
   (scores_hash[:player_score] == NUM_WINS) ||
-  (scores_hash[:computer_score] == NUM_WINS)
+    (scores_hash[:computer_score] == NUM_WINS)
 end
 
 def update_score(player, computer, scores_hash)
@@ -162,7 +151,7 @@ def valid_yes_no_quit(question)
   input = ''
   loop do
     prompt(question, false)
-    input = gets.chomp.downcase
+    input = gets.chomp.strip.downcase
     break if %w(y yes n no q quit).include?(input)
     prompt(messages("not_valid_input"))
     pause()
@@ -175,67 +164,49 @@ def win?(first, second)
 end
 
 # MAIN -----------------------------------------------------------------------
-system('clear') # clears the screen before starting the game
+system('clear')
 
-# Initialize "count" variable to keep track of the # of rounds
 round_num = 0
 
-# Define a hash to keep track of player scores
 scores = {
   player_score: 0,
   computer_score: 0
 }
 
-# Welcome user to the game and ask if they want to see the rules
 prompt(messages("welcome"))
 read_rules?()
 
-# Reiterate conditions of the game and ask if the user wants to begin playing
 prompt(messages("win_conditions") + "#{NUM_WINS}.")
 prompt(messages("how_to_quit"))
 pause()
 begin_game?()
 
-# "Load" the game
 prompt(messages("loading"))
 pause(4.0)
 
-# Main outer loop that continues until the player or computer score reaches
-# the required number of wins
 until someone_won?(scores)
-  # Ask the user if they want to continue (after they've played
-  # at least one round)
   continue() if round_num > 0
 
-  system('clear') # clears the screen before starting rounds
-  prompt(messages("dashes")) # formatting
+  system('clear')
+  prompt(messages("dashes"))
 
   round_num += 1
-  # Output the round number
   puts "ROUND NUMBER: #{round_num}"
 
-  # Randomly assign the computer a valid choice
   computer_choice = generate_computer_choice
 
-  # Let the user choose
   choice = let_user_choose()
 
-  # Output user and computer choices to user
   display_choices(choice, computer_choice)
 
-  # Display who won the round
   display_result(choice, computer_choice)
 
-  # Keep track of the score based on the winner of the round
   update_score(choice, computer_choice, scores)
 
-  # Output current user and computer score to user
   display_scores(scores)
 end
 
-# Output the grand winner
 prompt(messages("dashes")) # formatting
 display_grand_winner(scores)
 
-# Output the game has ended
 prompt(messages("goodbye"))
